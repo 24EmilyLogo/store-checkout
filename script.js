1a35d9bc340a12c4046edd612cacaedd00375542
StudentAM <studentam@Administrators-MacBook-Pro.local>
2023-11-08 10:29:29 -0800

    Thank You For the Music

---
+++ script.js
@@ -0,0 +1,18 @@
+const itemsCatalog = {
+    jsTxtbook:["689145740844", "JavaScript Textbook", 34.95],
+    xeroxPaper:["4549292070248", "Xerox Paper", 10.99],
+    firstAidKit:["092265222983", "First Aid Kit", 20.99],
+    boxOfPencils:["X002ELVL3J", "Box of Pencils (50ct.)", 15.99],
+    sanitizingWipes:["686024002468", "Sanitizing Wipes", 10.99],
+    n95Facemasks:["860004186236", "N95 Face Masks", 15.99],
+    kleenex:["036000214000","Kleenex", 3.99],
+    handSanitizer:["8809693254156", "Hand Sanitizer", 7.99],
+    printerPaper:["036500060480", "Printer Paper", 9.99],
+    brushPens:["085014561877", "Brush Pens", 10.99],
+    multiportAdapter:["X0032YGP2T", "Multiport Adapter", 25.99],
+    scissors:["B07G6JT1XS", "Scissors", 23.99],
+    iOSprgmngTxtbook:["9780134682334", "iOS Programming Textbook", 119.99],
+    spiralNotebook:["718103230759", "Spiral Notebook", 1.99]
+}
+// MEeP
+/* meEp */
